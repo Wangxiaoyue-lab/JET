37cf385478e1f7587dc87f56cf245306c08aff64
caodudu <caojundudu@qq.com>
2023-05-31 22:46:59 +0800

    .gitignore

--- utils_api.R
+++ utils_api.R
@@ -1,6 +1,6 @@
 utils_wilcox_cpp <- function(x, y) {
     library(Rcpp)
-    sourceCpp("./utils/cpp/fast_wilcox.cpp")
+    sourceCpp("./fast_wilcox.cpp")
     wilcoxon_test(x, y)
 }
 
@@ -8,13 +8,13 @@ utils_wilcox_cpp <- function(x, y) {
 utils_pca_cpp <- function(data, dims) {
     library(Rcpp)
     library(RcppArmadillo)
-    sourceCpp("./utils/cpp/fast_pca.cpp")
+    sourceCpp("./fast_pca.cpp")
     pca_cpp(data, dims)
 }
 
 utils_nmf_cpp <- function(V, k) {
     library(Rcpp)
     library(RcppArmadillo)
-    sourceCpp("./utils/cpp/fast_pca.cpp")
+    sourceCpp("./fast_pca.cpp")
     nmf_cpp(data, dims)
 }
