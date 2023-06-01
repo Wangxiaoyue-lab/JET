720cbab4654af020dd3f237a0d7ac2e3974eb69d
caodudu <caojundudu@qq.com>
2023-06-01 20:14:15 +0800

    modified

--- utils_api.R
+++ utils_api.R
@@ -1,7 +1,7 @@
 jet_path <- getwd()
 utils_wilcox_cpp <- function(x, y) {
     library(Rcpp)
-    sourceCpp(paste0(jet_path, "./fast_wilcox.cpp"))
+    sourceCpp(paste0(jet_path, "/fast_wilcox.cpp"))
     wilcoxon_test(x, y)
 }
 
@@ -9,13 +9,13 @@ utils_wilcox_cpp <- function(x, y) {
 utils_pca_cpp <- function(data, dims) {
     library(Rcpp)
     library(RcppArmadillo)
-    sourceCpp(paste0(jet_path, "./fast_pca.cpp"))
+    sourceCpp(paste0(jet_path, "/fast_pca.cpp"))
     pca_cpp(data, dims)
 }
 
 utils_nmf_cpp <- function(V, k) {
     library(Rcpp)
     library(RcppArmadillo)
-    sourceCpp(paste0(jet_path, "./fast_nmf.cpp"))
+    sourceCpp(paste0(jet_path, "/fast_nmf.cpp"))
     nmf_cpp(V, k)
 }
